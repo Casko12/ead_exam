ab62094a6387455ff7613787f5ed9b4296e7f3a1
Casko12 <phamduyhung.ftu@gmail.com>
2023-12-09 19:46:51 +0700

    Exam

--- src/main/java/com/example/ead_exam/service/impl/ProductServiceImpl.java
+++ src/main/java/com/example/ead_exam/service/impl/ProductServiceImpl.java
@@ -1,12 +1,20 @@
 package com.example.ead_exam.service.impl;
 
+import com.example.ead_exam.entity.Product;
+import com.example.ead_exam.repository.ProductRepository;
 import com.example.ead_exam.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ProductServiceImpl implements ProductService {
     @Autowired
-
+private ProductRepository productRepository;
+    @Override
+    public List<Product> findAllProduct() {
+        return productRepository.findAll();
+    }
 }
 
--- src/main/java/com/example/ead_exam/service/impl/SaleServiceImpl.java
+++ src/main/java/com/example/ead_exam/service/impl/SaleServiceImpl.java
@@ -1,4 +1,18 @@
 package com.example.ead_exam.service.impl;
 
-public class SaleServiceImpl {
+import com.example.ead_exam.entity.Sale;
+import com.example.ead_exam.repository.SaleRepository;
+import com.example.ead_exam.service.SaleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SaleServiceImpl implements SaleService {
+    @Autowired
+    private SaleRepository saleRepository;
+    @Override
+    public List<Sale> findAllSale() {
+        return saleRepository.findAll();
 }
