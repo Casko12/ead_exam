1039f92454f142dc49d5a250e9fbea3eeebf8c42
Casko12 <phamduyhung.ftu@gmail.com>
2023-12-09 19:52:53 +0700

    Exam

--- src/main/java/com/example/ead_exam/controller/ProductController.java
+++ src/main/java/com/example/ead_exam/controller/ProductController.java
@@ -1,4 +1,25 @@
 package com.example.ead_exam.controller;
 
+import com.example.ead_exam.entity.Product;
+import com.example.ead_exam.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/product")
 public class ProductController {
+    @Autowired
+    private ProductService productService;
+    @GetMapping
+    public String findAllSaleProduct(Model model) {
+        List<Product> products = productService.findAllProduct();
+        model.addAttribute("products", products);
+        return "product";
+    }
+
 }
--- src/main/java/com/example/ead_exam/controller/SaleController.java
+++ src/main/java/com/example/ead_exam/controller/SaleController.java
@@ -1,4 +1,24 @@
 package com.example.ead_exam.controller;
 
+import com.example.ead_exam.entity.Sale;
+import com.example.ead_exam.service.SaleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/sale")
 public class SaleController {
+    @Autowired
+    private SaleService saleService;
+    @GetMapping
+    public String findAllSaleProduct(Model model) {
+        List<Sale> sales = saleService.findAllSale();
+        model.addAttribute("sales", sales);
+        return "sale";
+    }
 }
